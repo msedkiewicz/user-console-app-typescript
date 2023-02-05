20b96a553ddaa397bf6b96bcfb517296990fa7f7
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-02-05 22:36:53 +0100

    Change app content

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 const inquirer = require('inquirer');
 
-inquirer.prompt([{
-    name: 'name',
-    type: 'input',
-    message: 'What\'s your name?',
-}, {
-    name: 'age',
-    type: 'number',
-    message: 'How old are you?',
-    default: 18,
-}]).then((answers: { name: string, age: number }) => {
-    console.log(`\nHi ${answers.name}. ${answers.age}? Nice! \n`);
-});
+const startApp = () => {
+    inquirer.prompt([{
+        name: 'action',
+        type: 'input',
+        message: 'How can I help you?',
+    }]).then((answers: { action: 'list' | 'add' | 'remove' | 'quit' }) => {
+        console.log("Chosen action: " + answers.action);
+        startApp();
+        if (answers.action === "quit")
+            return;
+    });
+}
+
+startApp();
